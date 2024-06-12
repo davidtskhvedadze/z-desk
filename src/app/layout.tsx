5011484dbe6635a93b2e3b9be7e92cd4540a6b3e
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-06-12 01:47:46 -0400

    Add all necessary pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { NavBar, NavLink } from "@/components/NavBar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -17,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("bg-background min-h-screen font-sans antialiased", inter.variable)}>{children}</body>
+      <body className={cn("bg-background min-h-screen font-sans antialiased", inter.variable)}>
+        <NavBar>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/admin">Admin</NavLink>
+        </NavBar>
+        {children}
+      </body>
     </html>
   );
 }
