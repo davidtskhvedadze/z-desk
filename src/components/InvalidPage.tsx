ab1a11bcbe810fe012a301dc2420daa5f03db6d8
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-06-14 20:14:30 -0400

    Shift loading page to root app

--- src/components/InvalidPage.tsx
+++ src/components/InvalidPage.tsx
@@ -5,7 +5,7 @@ import UnAuthorizedLogo from "../../public/unauthorized-user-access-svgrepo-com.
 
 export function InvalidPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <Image className="mx-auto" src={UnAuthorizedLogo} alt="Workflow" width={60} height={60}/>
